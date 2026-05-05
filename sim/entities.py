1e0c9129eb1737f72fa965fb5d0085d9cba0ec95
Chidis58 <39740634+Chidis58@users.noreply.github.com>
2026-05-05 05:42:59 +0100

    Add Connector and Node classes to entities.py

--- sim/entities.py
+++ sim/entities.py
@@ -1,1 +1,19 @@
 
+class Connector:
+    def __init__(self, id, balance, strategy):
+        self.id = id
+        self.balance = balance
+        self.connections = set()
+        self.conviction = {}
+        self.strategy = strategy
+
+
+class Node:
+    def __init__(self, id):
+        self.id = id
+        self.visit_load = 0
+        self.connections = set()
+        self.ring_holder = None
+        self.last_price = 10
+        self.earnings = 0
+        self.streak = 0
