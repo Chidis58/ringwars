8ffbada0c60d22ca9f7c78268883d4d64092fbdd
Chidis58 <39740634+Chidis58@users.noreply.github.com>
2026-05-05 05:45:24 +0100

    Add summarize function to calculate simulation metrics

--- sim/metrics.py
+++ sim/metrics.py
@@ -1,1 +1,10 @@
+def summarize(sim):
+    total_supply = sum(c.balance for c in sim.connectors.values())
+    avg_visit_load = sum(n.visit_load for n in sim.nodes.values()) / len(sim.nodes)
 
+    return {
+        "platform_revenue": sim.platform_revenue,
+        "burned": sim.total_burned,
+        "total_supply": total_supply,
+        "avg_visit_load": avg_visit_load,
+    }
